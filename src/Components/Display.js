223ce1f90f635cacef12b99f271c97e7e25c61c9
kvnal <72142540+kvnal@users.noreply.github.com>
2021-04-17 01:10:15 +0530

    sizes added

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -5,6 +5,7 @@ import { useState } from 'react';
 const Display = () => {
     const [text,setText]=useState({value:"Here.", color:"#fff",size:40})
     const [bg,setBg]=useState({color:"#000",width:1080,height:1080})
+    const sizes={coverPhoto : [1500,500], highligths:[1080,1920],sqaures : [1080,1080]} //width x hieght
 
      function saveImage() {
         const post = document.querySelector('#post');
@@ -37,11 +38,16 @@ const Display = () => {
                     <label htmlFor="bg">Background</label>
                     <input type="color" name="bg"   onChange={(e)=>setBg({...bg,color:e.target.value})} />
 
+                    <div>
+                        <button onClick={()=>setBg({...bg , width:sizes.coverPhoto[0] , height: sizes.coverPhoto[1]})}>coverPhoto</button>
+                        <button onClick={()=>setBg({...bg , width:sizes.highligths[0] , height: sizes.highligths[1]})}>highligths</button>
+                        <button onClick={()=>setBg({...bg , width:sizes.sqaures[0] , height: sizes.sqaures[1]})}>squares</button>
+                    </div>
                     </div>
                     <button onClick={saveImage}>Save</button>
                 </div>
             </div>
-        <div className="display" id="post" style={{ backgroundColor: bg.color , height:`${bg.height/3}px`, width: `${bg.width/3}px` }}>
+        <div className="display" id="post" style={{ backgroundColor: bg.color , height:`${bg.height/4}px`, width: `${bg.width/4}px` }}>
             <p className="text"  style={{color: text.color, fontSize:`${text.size}px`}} >{text.value}</p>
         </div>
         </div>
