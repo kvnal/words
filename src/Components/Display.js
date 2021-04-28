c9f91c0b7ab97f3d8cf6a389fa34e08ba2befe31
kvnal <72142540+kvnal@users.noreply.github.com>
2021-04-28 16:35:02 +0530

    padding range added

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -3,10 +3,10 @@ import { useState } from 'react';
 
 
 const Display = () => {
-    const [text,setText]=useState({value:"Here.", color:"#fff",size:24})
-    const [bg,setBg]=useState({color:"#000",width:1080,height:1080})
+    const [text,setText]=useState({value:"Here.", color:"#fff",size:24,padding:20})
+    const [bg,setBg]=useState({color:"#000",width:1080,height:1080,padding:20})
     const sizes={coverPhoto : [1500,500], highligths:[1080,1920],sqaures : [1080,1080]} //width x hieght
-
+    
      function saveImage(output) {
         const post = document.querySelector('#post');
         html2canvas(post,{scale:4}).then(canvas => {
@@ -20,6 +20,16 @@ const Display = () => {
 
     
     }
+
+     function loadFont(fontName){
+        
+        var link = document.createElement('link')
+        link.rel= 'stylesheet'
+        link.type='text/css'
+        link.href=`https://fonts.googleapis.com/css2?family=${fontName.split(' ').join('+')}&display=swap`
+        document.getElementsByTagName('head')[0].appendChild(link)
+
+    }
     return (
         <div className="displaygrid">
             <div>
@@ -30,25 +40,25 @@ const Display = () => {
 
                     <input type="text" defaultValue="here." placeholder="Text" onChange={(e)=>{setText( {...text,value : e.target.value})}}/>
                     <input type="color" className="color" defaultValue="#fff"  onChange={(e)=>setText({...text,color:e.target.value})} />
-                    <input type="number" placeholder="Text Size" step="2" defaultValue="40" onChange={(e)=>setText({...text,size:e.target.value})}/>
+                    <input type="number" placeholder="Text Size" step="2" defaultValue="24" onChange={(e)=>setText({...text,size:e.target.value})}/>
                     </div>
                     
                     <div>
-
                     <label htmlFor="bg">Background</label>
                     <input type="color" name="bg"   onChange={(e)=>setBg({...bg,color:e.target.value})} />
-
-                    <div>
-                        <button onClick={()=>setBg({...bg , width:sizes.coverPhoto[0] , height: sizes.coverPhoto[1]})}>coverPhoto</button>
-                        <button onClick={()=>setBg({...bg , width:sizes.highligths[0] , height: sizes.highligths[1]})}>highligths</button>
-                        <button onClick={()=>setBg({...bg , width:sizes.sqaures[0] , height: sizes.sqaures[1]})}>squares</button>
+                    <label htmlFor="padding">Text Padding</label>
+                    <input type="range" step='2' defaultValue='20' min="0" max="100" name="padding" onChange={(e)=>setText({...text,padding:e.target.value})} />
+                    <div >                        
+                        <button onClick={()=>setBg({...bg , width:sizes.coverPhoto[0] , height: sizes.coverPhoto[1]})}>coverPhoto (1.91:1)</button>
+                        <button onClick={()=>setBg({...bg , width:sizes.highligths[0] , height: sizes.highligths[1]})}>highligths (9:16)</button>
+                        <button onClick={()=>setBg({...bg , width:sizes.sqaures[0] , height: sizes.sqaures[1]})}>squares (1:1)</button>
                     </div>
                     </div>
                     <button onClick={()=>saveImage(text.value)}>Save</button>
                 </div>
             </div>
-        <div className="display" id="post" style={{ backgroundColor: bg.color , height:`${bg.height/4}px`, width: `${bg.width/4}px` }}>
-            <p className="text"  style={{color: text.color, fontSize:`${text.size}px`}} >{text.value}</p>
+        <div className="display" id="post" style={{ backgroundColor: bg.color , height:`${bg.height/4}px`, width: `${bg.width/4}px`}}>
+            <p className="text"  style={{color: text.color, fontSize:`${text.size}px`,padding:`0 ${text.padding}px 0 ${text.padding}px`,fontFamily: 'Zen Dots'}} >{text.value}</p>
         </div>
         </div>
     );
